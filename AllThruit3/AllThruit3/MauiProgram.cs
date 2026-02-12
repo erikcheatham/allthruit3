67f1f9751e05053d528e0b60365b01a66647f2c7
erk <751184+erikcheatham@users.noreply.github.com>
2026-02-12 02:44:02 -0600

    MAUI Rebuilt

--- AllThruit3/AllThruit3/MauiProgram.cs
+++ AllThruit3/AllThruit3/MauiProgram.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using AllThruit3.Services;
 using AllThruit3.Shared.Services;
-using AllThruit3.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace AllThruit3;
 
@@ -18,15 +19,15 @@ public static class MauiProgram
 
 
         // Build configuration: Load env vars from launchSettings.json (dev) or host (prod)
-        //builder.Configuration
-        //.AddEnvironmentVariables()
-        //.AddInMemoryCollection(new Dictionary<string, string?>
-        //    {
-        //        { "AppSettings:TMDBUrl", "https://api.themoviedb.org/3/" },
-        //        { "AppSettings:TMDBBearerToken", "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZWJiMWRmMDk3MjY1MDljMTdjZDBjNjIxZDU0MDkwYSIsIm5iZiI6MTc2MDg5NTMwMS4wMzksInN1YiI6IjY4ZjUyMTQ1NDI5NmNmMjRiNmY5OWY2MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5j0B4CHgvIFrgeLJLV8DddIrQD5l7ObDbo3u5hxqnlM" },
-        //        { "AppSettings:ApiBaseUrl", "https://localhost:7199/" }
-        //    })
-        //    .Build();
+        builder.Configuration
+        .AddEnvironmentVariables()
+        .AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                { "AppSettings:TMDBUrl", "https://api.themoviedb.org/3/" },
+                { "AppSettings:TMDBBearerToken", "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZWJiMWRmMDk3MjY1MDljMTdjZDBjNjIxZDU0MDkwYSIsIm5iZiI6MTc2MDg5NTMwMS4wMzksInN1YiI6IjY4ZjUyMTQ1NDI5NmNmMjRiNmY5OWY2MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5j0B4CHgvIFrgeLJLV8DddIrQD5l7ObDbo3u5hxqnlM" },
+                { "AppSettings:ApiBaseUrl", "https://localhost:7199/" }
+            })
+            .Build();
 
         // Add device-specific services used by the AllThruit3.Shared project
         builder.Services.AddSingleton<IFormFactor, FormFactor>();
